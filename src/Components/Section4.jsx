fe07b4d141d3f7113f93afe3c0522198ea26995b
Julius Fidelis <fidelisjulius70@gmail.com>
2026-09-12 10:26:06 +0100

    Fix About Us read more button logic and text formatting

--- src/Components/Section4.jsx
+++ src/Components/Section4.jsx
@@ -1,57 +1,41 @@
 import "./Section4.css"
-import { useState,useEffect } from "react"
-import "./Card.css"
+import { useState, useEffect } from "react"
 import AOS from "aos"
 import "aos/dist/aos.css"
 
 function AboutUs(){
-
-     useEffect(() => {
-         AOS.init({
-         duration: 1000, // animation duration in ms
-         easing: "ease-in-out", // easing function
-         once: true, // animation only happens once
-    });
-  }, []);
-
-   const  word = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero sapiente, nostrum assumenda quam debitis voluptatum doloremque voluptas quia reprehenderit fugiat quidem est illum eos ut magnam dolore dignissimos temporibus libero! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum cumque et numquam amet nisi fugiat fuga perspiciatis repellendus commodi eius! Ipsa iure architecto fuga. Provident accusantium explicabo nisi? Eos, ratione.Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero sapiente, nostrum assumenda quam debitis voluptatum doloremque voluptas quia reprehenderit fugiat quidem est illum eos ut magnam dolore dignissimos temporibus libero! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum cumque et numquam amet nisi fugiat fuga perspiciatis repellendus commodi eius! Ipsa iure architecto fuga. Provident accusantium explicabo nisi? Eos, ratione"
-
-    const [view,setView] = useState(false);
-    const [cut,setCut] = useState(word.slice(0,400))
-
-  
+    const [view, setView] = useState(false);
     
- function Words() {
-    setView(prevView => {
-        const newView = !prevView
+    useEffect(() => {
+        AOS.init({
+            duration: 1000,
+            easing: "ease-in-out",
+            once: true,
+        });
+    }, []);
 
-        if (newView) {
-        setCut(word.slice())          
-       } else {
-          setCut(word.slice(0, 400))    
-       }
+    const word = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero sapiente, nostrum assumenda quam debitis voluptatum doloremque voluptas quia reprehenderit fugiat quidem est illum eos ut magnam dolore dignissimos temporibus libero! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum cumque et numquam amet nisi fugiat fuga perspiciatis repellendus commodi eius! Ipsa iure architecto fuga. Provident accusantium explicabo nisi? Eos, ratione. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero sapiente, nostrum assumenda quam debitis voluptatum doloremque voluptas quia reprehenderit fugiat quidem est illum eos ut magnam dolore dignissimos temporibus libero! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum cumque et numquam amet nisi fugiat fuga perspiciatis repellendus commodi eius! Ipsa iure architecto fuga. Provident accusantium explicabo nisi? Eos, ratione.";
 
-    return newView
-  })
-}
+    function toggleView() {
+        setView(!view);
+    }
 
+    const displayWord = view ? word : word.slice(0, 400) + "...";
 
     return(
         <section className="about" id="about">
-            
             <h1 className="head">About Us</h1>
             <div className="min">
-                <div className="words" data-aos="slide-right"  data-aos-duration="1500" style={{fontFamily:"sans-serif",color:"#838080ff"}}>
-                    {cut}
-                    <button onClick={Words} className="btn3">Read More</button>
+                <div className="words" data-aos="slide-right" data-aos-duration="1500">
+                    <p>{displayWord}</p>
+                    <button onClick={toggleView} className="btn3">
+                        {view ? "Read Less" : "Read More"}
+                    </button>
                 </div>
                 
-
-            
                 <div className="image" data-aos="slide-left" data-aos-duration="1500">
-                    <img src="/whitedavistore/Images/Cover_Hero.jpg" alt="" />
+                    <img src="/whitedavistore/Images/Cover_Hero.jpg" alt="About Us" />
                 </div>
-
             </div>
         </section>
     )
